28ad03ffa9e79b0140ff0a6ebb03ec2f2a998dd5
VackKermit <pjs99047@gmail.com>
2018-12-29 22:06:20 +0900

    order_list(), order_cancle() are maked!

--- trader.py
+++ trader.py
@@ -1,19 +1,19 @@
 import time
 from pprint import pprint
 from urllib.parse import urlencode
-from requests import get, post
+from requests import get, post, delete
 from jwt import encode
 
-ACCESS_KEY = ""
-SECRET_KEY = ""
+ACCESS_KEY = "E10F3xqOUGAYxxFmExfGBo2xxx5Q1IsrpHqDszgx"
+SECRET_KEY = "1DjcP3cRxk7J4ymY4xkDxcnt9R1hXRT68FHDv3x3"
 
 # Start with "_" is private function normally
 # Implicit promise of no use in external modules
 
-####################################################
+##############################################################
 
 def get_account():
-    # 전체 계좌 조회
+    # check available for all accounts
     payload = {
         "access_key": ACCESS_KEY,
         "nonce": int(time.time() * 1000)
@@ -32,7 +32,7 @@ def get_account():
     return resp_data
 
 def orderable_info(market):
-    # 주문 가능 정보
+    # orderable information
     query = {
         "market": market
     }
@@ -56,7 +56,7 @@ def orderable_info(market):
     return resp_data
 
 def order(market, side, volume, price, ord_type):
-    # 주문
+    # order
     query = {
         "market": market,
         "side": side,
@@ -83,15 +83,72 @@ def order(market, side, volume, price, ord_type):
     resp_data = resp.json()
     return resp_data    
 
-def ticker(market):
-    url = "https://api.upbit.com/v1/ticker?markets=" + market
-    resp = get(url)
-    return resp.json()
+def order_list(state="wait", page=1):
+    query = {
+        "state": state,
+        "page": page
+    }
 
-####################################################    
+    query_encoded = urlencode(query)
+
+    payload = {
+        "access_key": ACCESS_KEY,
+        "nonce": int(time.time() * 1000),
+        "query": query_encoded
+    }
+
+    token = encode(payload, SECRET_KEY).decode()
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    url = "https://api.upbit.com/v1/orders?" + query_encoded
+    resp = get(url, headers=headers)
+    resp_data = resp.json()
+    return resp_data
+
+def order_cancle(uuid):
+    query = {
+        "uuid": uuid
+    }
+
+    query_encoded = urlencode(query)
+
+    payload = {
+        "access_key": ACCESS_KEY,
+        "nonce": int(time.time() * 1000),
+        "query": query_encoded
+    }
+
+    token = encode(payload, SECRET_KEY).decode()
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    url = "https://api.upbit.com/v1/order?" + query_encoded
+    resp = delete(url, headers=headers)
+    resp_data = resp.json()
+    return resp_data    
+
+def ticker(markets):
+    # ticker
+    url = "https://api.upbit.com/v1/ticker?markets=" + markets
+    resp = get(url)
+    resp_data = resp.json()
+    return resp_data
+
+def candle_minute(market):
+    # candle information for minute
+    url = "https://api.upbit.com/v1/candles/minutes/1?market=" + market
+    resp = get(url)
+    resp_data = resp.json()
+    return resp_data
+
+##############################################################
 
 def main():
-    pprint(ticker("KRW-BTC"))
+    resp = order_cancle("0d3173e5-3a5a-42bf-ab26-951918ac15e8")
+    pprint(resp)
 
 if __name__ == '__main__':
     main()
